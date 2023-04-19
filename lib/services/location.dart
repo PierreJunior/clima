71e1c886793766f5bec6024b2daf112c7baf2e14
PierreJunior <pierresangwa@gmail.com>
2023-04-19 12:23:54 +0200

    trying version number github actions

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -7,9 +7,8 @@ class Location {
 
   Future<void> getCurrentLocation() async {
     try {
-      LocationPermission givenPermission = await Geolocator.checkPermission();
-      LocationPermission requestPermission =
-          await Geolocator.requestPermission();
+      await Geolocator.checkPermission();
+      await Geolocator.requestPermission();
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high);
       latitude = position.latitude;
